03e96ed324545040421271fbc708c8ed2b86aa89
thewa1818 <zawa.i.0218@gmail.com>
2023-06-14 15:35:31 +0900

    3

--- js/app.js
+++ js/app.js
@@ -120,3 +120,30 @@ console.log(A[0][1]+A[2][0]);
 let mmm = inoki.concat(aizawa);
 
 console.log(mmm);
+
+
+
+const omi = ["大吉","中吉","吉","末吉","凶"];
+
+console.log(omi[1]);
+
+console.log(omi.length);
+
+omi.push ("大凶");
+
+omi.unshift("超大吉");
+
+console.log(omi);
+
+for (let i = 0; i < omi.length; i ++){
+  console.log(`結果は${omi[i]}です`);
+}
+
+const city = ["渋谷","練馬","六本木","表参道","曙橋","新宿"];
+
+let oo = ''
+for (let i = 1; i <= city.length; i++) {
+    oo += `<option id="tokyo">住所: 東京都${city[i]}</option>`
+};
+
+$("#tokyo").html(oo)
